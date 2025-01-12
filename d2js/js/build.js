581974a1f52b62ba992be84e8d7e598cf3f11b68
Alexander Wang <alex@terrastruct.com>
2025-01-12 12:07:44 -0700

    separate node esm and browser esm builds

--- d2js/js/build.js
+++ d2js/js/build.js
@@ -4,9 +4,9 @@ import { join } from "node:path";
 
 await mkdir("./dist/esm", { recursive: true });
 await mkdir("./dist/cjs", { recursive: true });
+await mkdir("./dist/browser", { recursive: true });
 
 const commonConfig = {
-  target: "node",
   splitting: false,
   sourcemap: "external",
   minify: true,
@@ -17,19 +17,32 @@ const commonConfig = {
   },
 };
 
-async function buildAndCopy(format) {
-  const outdir = `./dist/${format}`;
+await build({
+  ...commonConfig,
+  target: "node",
+  entrypoints: ["./src/index.js", "./src/worker.js", "./src/platform.js"],
+  outdir: "./dist/esm",
+  format: "esm",
+});
 
-  await build({
-    ...commonConfig,
-    entrypoints: ["./src/index.js", "./src/worker.js", "./src/platform.js"],
-    outdir,
-    format,
-  });
+await build({
+  ...commonConfig,
+  target: "node",
+  entrypoints: ["./src/index.js", "./src/worker.js", "./src/platform.js"],
+  outdir: "./dist/cjs",
+  format: "cjs",
+});
 
-  await copyFile("./wasm/d2.wasm", join(outdir, "d2.wasm"));
-  await copyFile("./wasm/wasm_exec.js", join(outdir, "wasm_exec.js"));
+await build({
+  ...commonConfig,
+  target: "browser",
+  entrypoints: ["./src/index.js", "./src/worker.js", "./src/platform.js"],
+  outdir: "./dist/browser",
+  format: "esm",
+});
+
+const dirs = ['esm', 'cjs', 'browser'];
+for (const dir of dirs) {
+  await copyFile("./wasm/d2.wasm", join(`./dist/${dir}`, "d2.wasm"));
+  await copyFile("./wasm/wasm_exec.js", join(`./dist/${dir}`, "wasm_exec.js"));
 }
-
-await buildAndCopy("esm");
-await buildAndCopy("cjs");
