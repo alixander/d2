8a7d818b065a253041fe027803ed8b7fe31a0b05
Alexander Wang <alex@terrastruct.com>
2025-08-11 11:38:33 -0600

    save

--- d2js/js/src/setup.js
+++ d2js/js/src/setup.js
@@ -5,6 +5,10 @@ var setTimeout = function (f) {
 console.log("setup.js: ELK available:", typeof ELK);
 var elk = new ELK();
 console.log("setup.js: Created elk instance:", typeof elk);
+
+// Alias layoutSync to layout for compatibility
+elk.layoutSync = elk.layout;
+
 globalThis.elk = elk;
 console.log("setup.js: Set globalThis.elk:", typeof globalThis.elk);
 console.log("setup.js: Finished execution");
